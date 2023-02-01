5adf9a7bc85c547af12c678c1d53a8bf0d42866e
Sherry He <xsherryhe@gmail.com>
2023-02-01 11:54:13 -0500

    Removed confirmable module from devise User model in consideration of planned production environment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_221728) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_01_165117) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,17 +37,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_221728) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "first_name"
     t.string "middle_name"
     t.string "last_name"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
