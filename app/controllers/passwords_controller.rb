e32c5fa518c847f7d69969456d0b0f7ecce5b7de
Sherry He <xsherryhe@gmail.com>
2023-03-06 19:14:10 -0500

    Complete and test reset password mailers, routes, and actions

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -11,4 +11,24 @@ class PasswordsController < Devise::PasswordsController
       respond_with_errors
     end
   end
+
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if resource_class.sign_in_after_reset_password
+        message = resource.active_for_authentication? ? :updated : :updated_not_active
+        resource.after_database_authentication
+        sign_in(resource_name, resource)
+      else
+        message = :updated_not_active
+      end
+      respond_with_message(message)
+    else
+      set_minimum_password_length
+      respond_with_errors
+    end
+  end
 end
